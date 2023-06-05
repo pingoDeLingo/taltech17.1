98fad1c4abe7e4e1df7f9c4dc806df7dc23ad9df
Robin <robin.noormets@voco.ee>
2023-06-05 23:44:17 +0300

    Tollid sentimeetriteks

---
+++ arvutused1.js
@@ -0,0 +1,14 @@
+var InchesToCm = /** @class */ (function () {
+    function InchesToCm() {
+    }
+    InchesToCm.prototype.calculate = function (inches) {
+        return inches * 2.54;
+    };
+    InchesToCm.prototype.inputUnit = function () {
+        return "in";
+    };
+    InchesToCm.prototype.outputUnit = function () {
+        return "cm";
+    };
+    return InchesToCm;
+}());
